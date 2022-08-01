ed9a328ad13ce8b2169ae7f7bae2e5a6ac2284db
Connor Bond <connorbond@driveworks.co.uk>
2022-08-01 12:07:31 +0100

    1.0.0 release

---
+++ config.js
@@ -0,0 +1,9 @@
+// Update these values to match those of your Server URL, DriveWorks Group Alias
+// and Project Name that you wish to be rendered.
+
+const config = {
+  serverUrl: "",
+  groupAlias: "CustomLoadingStates",
+  projectName: "CustomLoadingStates",
+  projectNameCustomLoadingElement: "CustomLoadingElement",
+};
